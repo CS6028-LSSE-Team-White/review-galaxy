9983050a2d8ce6c5212dc02e5979df09ab077e7e
Jack Margeson <margeson.jack@gmail.com>
2025-04-04 00:18:29 -0400

    Enable CORS

--- middleware/src/main/java/com/example/middleware/controller/IndexController.java
+++ middleware/src/main/java/com/example/middleware/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.middleware.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.time.Instant;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.example.middleware.model.APIStatus;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class IndexController {
 
